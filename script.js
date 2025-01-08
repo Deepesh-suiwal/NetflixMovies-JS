1f7bbced7f0fc028d9d08e98fca6c001886adf49
Deepesh <sainideepesh12@gmail.com>
2025-01-08 17:21:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+import requests from "./data.js";
+
+const contentDiv = document.querySelector(".content");
+const keys = Object.keys(requests);
+const updatedKeys = keys.map((key) => key.slice(5).toLowerCase());
+// console.log(updateKeys);
+
+const base_url = "https://api.themoviedb.org/3";
+const img_base_path = "https://image.tmdb.org/t/p/original";
+const data = [];
+
+for (let x in requests) {
+    const promise = getDataFromURL(base_url + requests[x]);
+    data.push(promise);
+}
+
+const finalData = await promise.all(data);
+displayData(finalData);
+
+function displayData(data) {
+    data.forEach((object, index) => {
+        const movieWrapper = document.createElement("div");
+        movieWrapper.classList.add("movieWrapper");
+        object.results.forEach((movie) => {
+            const parent = document.createElement("div");
+            parent.classList.add("parent");
+            const image = document.createElement("img");
+            image.src = img_base_path + movie.poster_path;
+            parent.append(image);
+            movieWrapper.append(parent);
+        });
+        document.querySelector("#" + updatedKeys[index]).append(movieWrapper);
+    })
+}
